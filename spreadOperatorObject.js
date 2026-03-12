f3e6953ba127515c8853b79226c4176cb086dea0
bhavitha-111 <bhavithakammara@gmail.com>
2026-03-12 20:31:50 +0530

    Delete spreadOperatorObject.js

--- spreadOperatorObject.js
+++
@@ -1,8 +0,0 @@
-const defaults = { theme: 'dark', lang: 'en', font: 'mono' };
-const userPrefs = { lang: 'hi', fontSize: 14 };
-// Merge (later keys win)
-const config = { ...defaults, ...userPrefs };
-// { theme: 'dark', lang: 'hi', font: 'mono', fontSize: 14 }
-// Clone object (shallow)
-const clone = { ...defaults }  
-console.log(clone);

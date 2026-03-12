28f804ff9c438f69420e247e6a14cc2806972523
bhavitha-111 <bhavithakammara@gmail.com>
2026-03-12 20:29:55 +0530

    Delete async.js

--- async.js
+++
@@ -1,9 +0,0 @@
-async function greet() {
-return 'Hello!';
-}
-// Same as:
-function greet() {
-return Promise.resolve('Hello!');
-}
-// Both are consumed the same way:
-greet().then(msg => console.log(msg)); // 'Hello!'

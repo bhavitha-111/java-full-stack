15a9595f87b8d2d8d87a775b0d84416d3ac9b6df
bhavitha-111 <bhavithakammara@gmail.com>
2026-03-12 20:30:33 +0530

    Delete callback.js

--- callback.js
+++
@@ -1,9 +0,0 @@
-// A function that accepts a callback
-function greetUser(name, callback) {
-const message = `Hello, ${name}!`;
-callback(message);
-}
-// Passing a function as an argument
-greetUser('Bhavitha', function(msg) {
-console.log(msg); // 'Hello, Lalita!'
-});
